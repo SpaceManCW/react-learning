e408c86b126f4164eb311f2467939aaecfbd4505
linchuanwei1 <linchuanwei1@jd.com>
2023-11-29 17:50:29 +0800

    样式

--- src/App.js
+++ src/App.js
@@ -7,37 +7,51 @@ import { Link } from 'react-router-dom'
 import './App.css'
 import routes from './router/config'
 import RouterView  from './router/index'
+
 const { SubMenu } = Menu;
 const App = () => {
+    let pathname = window.location.pathname
+    let defaulS = []
+    defaulS.push(pathname)
+    const handleClick = (e) => {
+        defaulS.pop()
+        defaulS.push(e.key)
+    }
     return (
-        <div>
-            <Menu mode="horizontal">
-                <Menu.Item icon={<MailOutlined />} key = 'd1'>
+        <div className='container'>
+            <Menu 
+              mode="inline" 
+              className='menu' 
+              theme='light' 
+              defaultSelectedKeys={defaulS}
+              onClick={handleClick}
+            >
+                <Menu.Item icon={<MailOutlined />} key = '/demo01'>
                     <Link to = "/demo01">类组件的使用</Link>
                 </Menu.Item>
-                <Menu.Item icon={<AppstoreOutlined />} key = 'd2'>
+                <Menu.Item icon={<AppstoreOutlined />} key = '/demo02'>
                     <Link to = "/demo02">获取组件的值</Link>
                 </Menu.Item>
-                <Menu.Item icon={<AppstoreOutlined />} key = 'd3'>
+                <Menu.Item icon={<AppstoreOutlined />} key = '/demo03'>
                     <Link to = "/demo03">评论</Link>
                 </Menu.Item>
-                <Menu.Item icon={<AppstoreOutlined />} key = 'd4'>
+                <Menu.Item icon={<AppstoreOutlined />} key = '/demo04'>
                     <Link to = "/demo04">Context的使用方法</Link>
                 </Menu.Item>
-                <Menu.Item icon={<AppstoreOutlined />} key = 'd5'>
+                <Menu.Item icon={<AppstoreOutlined />} key = '/demo05'>
                     <Link to = "/demo05">父对子传参</Link>
                 </Menu.Item>
-                <Menu.Item icon={<AppstoreOutlined />} key = 'd6'>
+                <Menu.Item icon={<AppstoreOutlined />} key = '/demo06'>
                     <Link to = "/demo06">子对父传参</Link>
                 </Menu.Item>
                 <SubMenu title="React Hooks" icon={<AppstoreOutlined />} key="SubMenu">
-                    <Menu.Item key = 'd8'>
+                    <Menu.Item key = '/demo08'>
                         <Link to = "/demo08">useState&&useContext</Link>
                     </Menu.Item>
-                    <Menu.Item key = 'd9'>
+                    <Menu.Item key = '/demo09'>
                         <Link to = "/demo09">useReducer</Link>
                     </Menu.Item>
-                    <Menu.Item key = 'd10'>
+                    <Menu.Item key = '/demo10'>
                         <Link to = "/demo10">useEffect</Link>
                     </Menu.Item>                        
                 </SubMenu>
